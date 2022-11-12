c97decd8dbf258bd80fb87d5dd9d981ea637568c
Luke Tang <tanlu824@student.otago.ac.nz>
2022-11-12 17:03:02 +1300

    Add movement to asteroids

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -7,40 +7,65 @@ public class Asteroid : MonoBehaviour
 
     public Transform asteroidPrefab;
 
-    private bool shotOnce = false;
+    public int minFloatSpeed = 40;
+
+    public int maxFloatSpeed = 50;
+
+    private int floatSpeed = 50;
+
+    private bool isChild = false;
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        rb = GetComponent<Rigidbody2D>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        floatSpeed = Random.Range(40, 50);
+
+        // Hurtle towards center of screen
+        Vector2 centerDirection = new Vector2(0 - transform.position.x, 0 - transform.position.y);
+        centerDirection.Normalize();
+        rb.AddForce(centerDirection * floatSpeed);
+        // TODO: Add more variety to all the possible initial directions
     }
 
     void OnCollisionEnter2D(Collision2D other) 
     {
-        if (!shotOnce)   
+        if (other.gameObject.tag == "Asteroid")
         {
+            return;
+        }
+
+        if (!isChild)   
+        {
+            Vector2 randomDirection;
+
             Transform firstAsteroid = Instantiate(asteroidPrefab);
-            firstAsteroid.rotation = new Quaternion(firstAsteroid.rotation.x, firstAsteroid.rotation.y, Random.Range(0, 360), firstAsteroid.rotation.w);
+            firstAsteroid.rotation = new Quaternion(firstAsteroid.rotation.x, firstAsteroid.rotation.y, Random.Range(0.0f, 360.0f), firstAsteroid.rotation.w);
             firstAsteroid.localScale /= 2.0f;
-            firstAsteroid.GetComponent<Asteroid>().SetShotOnce(true);
+            randomDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+            randomDirection.Normalize();
+            firstAsteroid.GetComponent<Rigidbody2D>().AddForce(randomDirection * floatSpeed);
+            firstAsteroid.GetComponent<Asteroid>().SetIsChild(true);
 
             Transform secondAsteroid = Instantiate(asteroidPrefab);
-            secondAsteroid.rotation = new Quaternion(secondAsteroid.rotation.x, secondAsteroid.rotation.y, Random.Range(0, 360), secondAsteroid.rotation.w);
+            secondAsteroid.rotation = new Quaternion(secondAsteroid.rotation.x, secondAsteroid.rotation.y, Random.Range(0.0f, 360.0f), secondAsteroid.rotation.w);
             secondAsteroid.localScale /= 2.0f;
-            secondAsteroid.GetComponent<Asteroid>().SetShotOnce(true);
+            randomDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+            randomDirection.Normalize();
+            secondAsteroid.GetComponent<Rigidbody2D>().AddForce(randomDirection * floatSpeed);
+            secondAsteroid.GetComponent<Asteroid>().SetIsChild(true);
+
+            Physics2D.IgnoreCollision(firstAsteroid.GetComponent<Collider2D>(), secondAsteroid.GetComponent<Collider2D>());
         }
 
         Destroy(gameObject);
     }
 
-    public void SetShotOnce(bool state)
+    public void SetIsChild(bool state)
     {
-        shotOnce = state;
+        isChild = state;
     }
 }
