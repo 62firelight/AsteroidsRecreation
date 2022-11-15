35b0e89830e996a6ebe5c07d791a72b5cff85970
Luke Tang <tanlu824@student.otago.ac.nz>
2022-11-15 14:07:12 +1300

    Add spawn system for initial asteroid wave

--- Assets/Scripts/LevelMaster.cs
+++ Assets/Scripts/LevelMaster.cs
@@ -5,28 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class LevelMaster : MonoBehaviour
 {
+    public Transform asteroidPrefab;
+
+    public int initialNumberOfAsteroids = 4;
+
+    private int numberOfAsteroids;
+
     // Start is called before the first frame update
     void Start()
     {
         // Ensure that the level master object is always centered on the origin
         transform.position = Vector3.zero;
 
-        EdgeCollider2D[] edges = GetComponents<EdgeCollider2D>();
+        // Get coordinates for bottom-left point and top-right point of screen
+        Vector2 bottomLeftPoint = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        Vector2 topLeftPoint = Camera.main.ScreenToWorldPoint(new Vector3(0, Camera.main.pixelHeight, 0));
+        Vector2 topRightPoint = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight, 0));
+        Vector2 bottomRightPoint = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0, 0));
 
+        // Create boundary points for each edge of the screen
+        Vector2[] leftBoundaryPoints = new Vector2[] { bottomLeftPoint, topLeftPoint };
+        Vector2[] topBoundaryPoints = new Vector2[] { topLeftPoint, topRightPoint };
+        Vector2[] rightBoundaryPoints = new Vector2[] { topRightPoint, bottomRightPoint };
+        Vector2[] bottomBoundaryPoints = new Vector2[] { bottomRightPoint, bottomLeftPoint };
+        Vector2[][] boundaries = new Vector2[][] { leftBoundaryPoints, topBoundaryPoints, rightBoundaryPoints, bottomBoundaryPoints };
+
+        // Spawn asteroids
+        numberOfAsteroids = initialNumberOfAsteroids;
+        for (int i = 0; i < numberOfAsteroids; i++) 
+        {
+            // Select random boundary edge
+            int randomBoundaryIndex = Random.Range(0, boundaries.Length);
+            Vector2[] randomBoundary = boundaries[randomBoundaryIndex];
+
+            // Select random position on the selected boundary edge
+            Vector2 firstPos = randomBoundary[0];
+            Vector2 secondPos = randomBoundary[1];
+            Vector2 randomPos = new Vector2(Random.Range(firstPos.x, secondPos.x), Random.Range(firstPos.y, secondPos.y));
+
+            // Add small offset to create variety in random positions
+            randomPos += new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+
+            Instantiate(asteroidPrefab, randomPos, transform.rotation);
+        }
+
+        // Set up screen boundaries
+        // Currently disabled
+        EdgeCollider2D[] edges = GetComponents<EdgeCollider2D>();
         if (edges.Length == 4)
         {
-            // Get coordinates for bottom-left point and top-right point of screen
-            Vector2 bottomLeftPoint = Camera.main.ScreenToWorldPoint(Vector3.zero);
-            Vector2 topLeftPoint = Camera.main.ScreenToWorldPoint(new Vector3(0, Camera.main.pixelHeight, 0));
-            Vector2 topRightPoint = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight, 0));
-            Vector2 bottomRightPoint = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0, 0));
-
-            // Create boundary points for each edge of the screen
-            Vector2[] leftBoundaryPoints = new Vector2[] { bottomLeftPoint, topLeftPoint };
-            Vector2[] topBoundaryPoints = new Vector2[] { topLeftPoint, topRightPoint };
-            Vector2[] rightBoundaryPoints = new Vector2[] { topRightPoint, bottomRightPoint };
-            Vector2[] bottomBoundaryPoints = new Vector2[] { bottomRightPoint, bottomLeftPoint };
-
             // Set boundary points
             edges[0].points = leftBoundaryPoints;
             edges[1].points = topBoundaryPoints;
