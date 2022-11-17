47d9e8a808bc23e8de3d3981be8f0030bd904ed1
Luke Tang <tanlu824@student.otago.ac.nz>
2022-11-17 13:23:05 +1300

    Remove comma from score/lives text

--- Assets/Scripts/LevelMaster.cs
+++ Assets/Scripts/LevelMaster.cs
@@ -100,7 +100,7 @@ public class LevelMaster : MonoBehaviour
         GUI.skin.label.alignment = TextAnchor.UpperCenter;
         if (GameMaster.playerLives > 0)
         {
-            GUI.Label(new Rect(0, 0, Camera.main.pixelWidth, Camera.main.pixelHeight), "Score: " + GameMaster.score + ", Lives: " + GameMaster.playerLives);
+            GUI.Label(new Rect(0, 0, Camera.main.pixelWidth, Camera.main.pixelHeight), "Score: " + GameMaster.score + "    Lives: " + GameMaster.playerLives);
         }
         else
         {
